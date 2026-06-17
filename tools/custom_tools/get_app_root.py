2ecafbea7986e7101be5f87d570b57df059c01b3
Taran Prabhu <taranprabhu1@gmail.com>
2026-06-17 16:45:43 -0700

    Fix packaged app FFmpeg path

--- tools/custom_tools/get_app_root.py
+++ tools/custom_tools/get_app_root.py
@@ -4,6 +4,6 @@ from pathlib import Path
 
 def get_app_root():
     if getattr(sys, "frozen", False):
-        return Path(sys.executable).parent
+        return Path(getattr(sys, "_MEIPASS", Path(sys.executable).parent))
 
     return Path(__file__).resolve().parents[2]
